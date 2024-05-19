f1d46c2b0bd6a34fc63dbda3231e7311b4d32fff
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-05-19 20:41:55 +0545

    Add files via upload

---
+++ scrip1.js
@@ -0,0 +1,19 @@
+// let iWillComeClass = true
+// if(iWillComeClass){
+//    console.log("Patho book")
+// } else{
+//     console.log("Back to sleep")
+// }
+
+let stock = 1000
+let age = 19
+let ashutosh = true
+let paisa = 120
+
+if(stock >= 1 && age>= 19 && ashutosh && paisa > 190 ){
+    console.log("Your order is ready!")
+
+}
+else{
+    console.log("Not available")
+}
---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("Hello World!, I am From external file");//console ma print huncha
+document.write("Hello! I am <b> Sweta</b>");
+document.write("<ul><li>List</li></ul>")
+~
+document.write(`Hello 
+I 
+Am 
+Sweta`)
+// ASI= AUTOMATIC SEMICOLON INSTRUCTIONS
+//static and dynamic programming types
+//static language = harek varibale ko define- Tthis type sade. in C, int = 1
+//dynamic language = variable type huncha- 
+
+
+// let name = prompt("HEllo what is your name")
+// console.log(name)
+
+
+// console.log(5>4)
+// console.log(4===4)
+// console.log(5 !== 4)
+
+let isTodayHot = true
+let isTodayWorkDay = true
+let isTodayClass = true
+
+
+let classcondition = isTodayClass && isTodayWorkDay && !isTodayHot
